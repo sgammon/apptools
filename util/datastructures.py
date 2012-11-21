39a82e1ccec925c1a74d4bf410a94bf3f8dd9b08
Sam Gammon <sam@openfi.re>
2012-11-21 01:07:32 -0800

    Typo in datastructures...

--- util/datastructures.py
+++ util/datastructures.py
@@ -442,7 +442,7 @@ class ProxiedStructure(type):
 
         new_cls = type(name, chain, mappings)
         if mappings.get('__singleton__') is True:
-            return object.__new__(cls)
+            return object.__new__(new_cls)
         else:
             return new_cls
 
