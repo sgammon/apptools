855a9db7a986fad28497e3257a688e2b6274de72
Sam Gammon <sam.gammon@ampush.com>
2013-05-26 18:18:52 -0700

    New `memoize` utility decorator, further structure to the RPC 2.0 rewrite.

--- rpc/__init__.py
+++ rpc/__init__.py
@@ -38,6 +38,7 @@ from apptools import model
 from apptools.util import json
 from apptools.util import debug
 from apptools.util import platform
+from apptools.util import decorators
 from apptools.util import datastructures
 
 
@@ -74,8 +75,8 @@ try:
 
     # remote / message packages
     from protorpc import remote
-    from protorpc.remote import method
-    from protorpc.remote import Service
+    from protorpc.remote import method as proto_method
+    from protorpc.remote import Service as ProtoService
 
     # message packages
     from protorpc import messages as pmessages
@@ -88,7 +89,7 @@ try:
 
     # WSGI handlers
     from protorpc import wsgi
-    from protorpc.wsgi import service_mappings
+    from protorpc.wsgi import service as gateway
 
 except:
 
@@ -112,7 +113,7 @@ else:
     _DEFAULT_BASEPATH = '/_api'
     _DEFAULT_REGISTRY_PATH = _DEFAULT_BASEPATH + '/registry'
     _DEFAULT_OAUTH_SCOPES = ("https://www.googleapis.com/auth/userinfo.email", "https://www.googleapis.com/auth/userinfo.profile")
-    _DEFAULT_OAUTH_AUDIENCES = (endpoints.API_EXPLORER_CLIENT_ID,) if endpoints else None
+    _DEFAULT_OAUTH_AUDIENCES = (endpoints.API_EXPLORER_CLIENT_ID,) if endpoints else '292824132082.apps.googleusercontent.com'
 
     # allow override of above constants via config
     if config and len(srvconfig):
@@ -166,6 +167,7 @@ else:
         namespace = pmessages.StringField(5)  # string namespace for key
         parent = pmessages.MessageField('Key', 6)  # recursive key message for parent
 
+
     ## Echo - valid as a request as a response, simply defaults to 'Hello, world!'. Mainly for testing.
     class Echo(ProtoMessage):
 
@@ -178,11 +180,122 @@ else:
     messages = datastructures.DictProxy(**{
 
         # apptools-provided messages
-        'Key': Key,
-        'Echo': Echo,
+        'Key': Key,  # message class for an apptools model key
+        'Echo': Echo,  # echo message defaulting to `hello, world` for testing
 
         # builtin messages
-        'Message': ProtoMessage,
-        'VoidMessage': ProtoVoidMessage,
+        'Message': ProtoMessage,  # top-level protorpc message class
+        'VoidMessage': ProtoVoidMessage,  # top-level protorpc void message
+
+        # specific types
+        'Enum': pmessages.Enum,  # enum descriptor / definition class
+        'Field': pmessages.Field,  # top-level protorpc field class
+        'FieldList': pmessages.FieldList,  # top-level protorpc field list class
+
+        # field types
+        'VariantField': VariantField,  # generic hold-anything property (may cause serializer problems - be careful)
+        'BooleanField': pmessages.BooleanField,  # boolean true/false field
+        'BytesField': pmessages.BytesField,  # low-level binary-safe string field
+        'EnumField': pmessages.EnumField,  # field for referencing an :py:class:`pmessages.Enum` class
+        'FloatField': pmessages.FloatField,  # field for a floating point number
+        'IntegerField': pmessages.IntegerField,  # field for an integer
+        'MessageField': pmessages.MessageField,  # field for a sub-message (:py:class:`pmessages.Message`)
+        'StringField': pmessages.StringField  # field for unicode or ASCII strings
 
     })
+
+    # detect newest version of protorpc
+    if hasattr(pmessage_types, 'DateTimeField'):
+        messages['DateTimeField'] = pmessage_types.DateTimeField
+    elif _DEBUG:
+        # notify the user in debug mode that `datetime` support will get crappy
+        logging.warning('Detected out-of-date `ProtoRPC` library. Failed to load `datetime` support.')
+        logging.debug('Python `datetime` values will be converted to dumb `isoformat`.')
+
+    #### ==== Service Classes ==== ####
+
+    ## RemoteServiceFactory - class for generating/preparing new RemoteService objects
+    class RemoteServiceFactory(object):
+
+        ''' Responsible for manufacturing BaseService classes. '''
+
+        @classmethod
+        def new(self, service):
+
+            ''' Return the service, unmodified (this will be used later).
+
+                :param service: Service class to initialize. Must be a
+                                descendent of :py:class:`remote.Service`.
+
+                :returns: Constructed / materialized descendent of
+                          :py:class:`remote.Service`. '''
+
+            return service
+
+
+    ## Service - top-level base class for remote service classes.
+    @platform.PlatformInjector
+    class Service(ProtoService):
+
+        ''' Top-level parent class for ProtoRPC-based apptools services. '''
+
+        # == Public Properties == #
+        name = None
+        version = None
+        enabled = True
+
+        # == Internal Properties == #
+        _version = (0, 5)
+        _config_path = 'apptools.rpc.Service'
+
+        # Encapsulator references
+        handler = None
+        request = {}
+        middleware = {}
+
+        # Mapped exceptions
+        exceptions = datastructures.DictProxy({
+            'ApplicationError': remote.ApplicationError
+        })
+
+        # Jinja2 integration
+        context = {}
+        context_injectors = []
+
+        @decorators.memoize
+        @decorators.classproperty
+        def config(cls):
+
+            ''' Resolve :py:class:`Service`-specific config.
+
+                :returns: Configuration ``dict`` for the current
+                          :py:class:`Service` class, if any. '''
+
+            return srvconfig or {}
+
+        @decorators.memoize
+        @decorators.classproperty
+        def logging(cls):
+
+            ''' Generate a dedicated logging pipe.
+
+                :returns: An instance of :py:class:`debug.AppToolsLogger`,
+                          customized for the current :py:class:`Service`
+                          class. '''
+
+            _psplit = cls._config_path.split('.')
+            return debug.AppToolsLogger(**{
+                'path': '.'.join(_psplit[0:-1]),
+                'name': _psplit[-1]})._setcondition(cls.config.get('debug', True))
+
+        def initialize_request_state(self, state):
+
+            ''' Request state hook from ``ProtoRPC``.
+
+                :param state: State object handed in from ``ProtoRPC``,
+                              which should be a descendent of the class
+                              :py:class:`protorpc.remote.RequestState`. '''
+
+            pass
+
+
--- util/decorators.py
+++ util/decorators.py
@@ -1,12 +1,93 @@
-from apptools.util import datastructures
+# -*- coding: utf-8 -*-
+
+"""
+-----------------------------
+apptools2: utility decorators
+-----------------------------
+
+this package provides useful decorators that crosscut the regular
+functional bounds of apptools' main packages. stuff in here is
+generally used everywhere.
+
+:author: Sam Gammon (sam@momentum.io)
+:copyright: (c) 2013 momentum labs.
+:license: This is private source code - Ampush has been granted an
+          unlimited, exclusive license for embedded use. For details
+          about embedded licenses and other legalese, see `LICENSE.md`.
+"""
 
 
+## ``classproperty`` - use like ``@property``, but at the class-level.
 class classproperty(property):
 
-    ''' Custom decorator for class-level property getters. '''
+    ''' Custom decorator for class-level property getters.
+        Usable like ``@property`` and chainable with
+        ``@memoize``, as long as ``@memoize`` is used as
+        the inner decorator. '''
 
     def __get__(self, instance, owner):
 
-        ''' Return the property value at the class level. '''
+        ''' Return the property value at the class level.
+
+            :param instance: Current encapsulating object
+                             dispatching via the descriptor
+                             protocol, ``None`` if we are
+                             being dispatched from the
+                             class level.
+
+            :param owner: Corresponding owner type, available
+                          whether we're dispatching at the
+                          class or instance level.
+
+            :returns: Result of a ``classmethod``-wrapped,
+                      ``property``-decorated method. '''
 
         return classmethod(self.fget).__get__(None, owner)()
+
+
+## ``memoize`` - cache the output of a property descriptor call
+class memoize(property):
+
+    ''' Custom decorator for property memoization. Usable
+        like ``@property`` and chainable with ``@classproperty``,
+        the utility decorator above. '''
+
+    _value = None
+    __initialized__ = False
+
+    def __get__(self, instance, owner):
+
+        ''' If we have a cached value attached to this
+            context, return it.
+
+            :param instance: Current encapsulating object
+                             dispatching via the descriptor
+                             protocol, or ``None`` if we
+                             are being dispatched from the
+                             class level.
+
+            :param owner: Owner type for encapsulating
+                          object, if dispatched at the
+                          instance level.
+
+            :raises: Re-raises all exceptions encountered
+                     in the case of an unexpected state during
+                     delegated property dispatch.
+
+            :returns: Cached value, if any. If there is
+                      no cached value, defers to decorated
+                      method. '''
+
+        if not self.__initialized__:
+
+            try:
+                if isinstance(self.fget, classproperty):
+                    self._value = classmethod(self.fget.fget).__get__(None, owner)()
+                else:
+                    self._value = self.fget.__get__(instance, owner)()
+            except:
+                raise
+            else:
+                self.__initialized__ = True
+
+        return self._value
